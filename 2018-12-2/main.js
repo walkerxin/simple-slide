ecd3584bee46f1f73b84f811f73702e50627cabf
walkerxin <1464193658@qq.com>
2018-12-02 11:51:57 +0800

    悬浮/移出时，暂停/重新播放

--- 2018-12-2/main.js
+++ 2018-12-2/main.js
@@ -2,29 +2,43 @@ var $allBtn = $('.button span')
 for (var i=0; i<$allBtn.length; i++) {
     $allBtn[i].onclick = function (x) {
         var $theBtn = $(x.currentTarget || x.target)
+        // x为浏览器传递进来的触发元素信息，
+        // 使用 trigger 触发事件时，x.currentTarget为undefined
         // 获取此节点是其爸爸的第几个子元素：$('x').index()
-        var offset = $theBtn.index() * (-400)
+        n = $theBtn.index()
+        var offset = n * (-400)
         nextSlide($theBtn, offset)
     }
 }
 
-// 自动轮播
 var n = 0
 var size = $allBtn.length
-activeBtn(n % size)
 
-var timerId = setInterval(function () {
-    n += 1
-    activeBtn(n % size)
-}, 1200)
+// 自动轮播
+var timerId = setTimer()
+
+$('.images-window').on({
+    mouseenter: function(){
+        window.clearInterval(timerId)
+    },
+    mouseleave: function(){
+        timerId = setTimer()
+    }
+})
 
 function activeBtn(index) {
-    //   window['item'+(n % size + 1)].click()  
-    //   $allBtn.eq(index)[0].click()
     $allBtn.eq(index).trigger('click')
 }
 
 function nextSlide($theBtn, offset) {
     $('.images').css('transform', 'translateX(' + offset + 'px)')
     $theBtn.addClass('red').siblings('.red').removeClass('red')
+}
+
+function setTimer() {
+    return setInterval(function () {
+        n += 1
+        activeBtn(n % size)
+    }, 3000)
+
 }
